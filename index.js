ac9badfc15cd0b46eea9694cbb4647a05ccbc1b1
Aanchal <56544911+Aanchal1012@users.noreply.github.com>
2025-02-27 23:51:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,4 +7,4 @@ setInterval(async ()=>{
     // console.log(`Got keep-alive for stories! - `,(resp.data))
     const resp2 = await axios.get('https://vanaj-backend-dev.onrender.com/');
     console.log(`Got keep-alive for vanaj! - `,(resp2.data))
-}, 14)
+}, 14*1000*60)
